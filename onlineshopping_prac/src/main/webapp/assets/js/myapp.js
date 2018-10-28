b4f9e91183e0b5728b3a25af3a20b9716b88f960
Harshana Tirlotkar <harshanatirlotkar@gmail.com>
2018-10-28 16:00:44 +0530

    static and theme is added

---
+++ onlineshopping_prac/src/main/webapp/assets/js/myapp.js
@@ -0,0 +1,19 @@
+$(function(){
+//	solving active menu problem
+
+	switch(menu){
+	case 'About Us' :
+		$('#about').addClass('Active');
+		break;
+	case 'Contact Us' :		
+		$('#contact').addClass('Active');
+		break;
+	default:
+		$('#home').addClass('Active');
+	break;
+	}
+
+
+
+
+});
